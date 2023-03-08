fc7c770c622badc22a931c33f2e527b7d1f34b80
Leo <santos-contato@hotmail.com.br>
2023-03-08 04:39:12 -0300

    PROJETO CONCLUIDO

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const form  = document.getElementById('form');
+const form = document.getElementById('form');
 form.addEventListener('submit', handleSubmit);
 
 let status = document.getElementById('status');
@@ -7,8 +7,9 @@ let result = document.getElementById('result');
 
 const Guess ={
 max:10,
-attemptNumber:0,
-numberDrawn:function randomValue (){return Math.round(Math.random*this.max)}
+attemptsNumber:0,
+numberDrawn:function randomValue (){
+    return Math.round(Math.random() * this.max)}
 };
 
 let numberDrawn = Guess.numberDrawn();
@@ -23,18 +24,18 @@ e.preventDefault();
 let kick = document.getElementById('kick').value;
 
 if (!kick){
-    alert('Digite Algum Valor')
+    alert('Digite Algum Valor !')
     return;
 };
-updateAttempt(attempt, ++ Guess.attemptNumber);
+
+updateAttempt(attempt, ++Guess.attemptsNumber);
 
 if(numberDrawn == kick){
     playAgain();
     status.innerHTML = 'Parabéns, vocÊ acertou !';
-    result.styles.transition = '0.4s';
-    result.styles.backgroundColor = '#37c978';
-    result.styles.color ='#fff';
-    status.styles.color = '#fff';
+    result.style.backgroundColor = '#37c978';
+    result.style.color ='#fff';
+    status.style.color = '#fff';
     clear();
 } else if(numberDrawn > kick){
     status.innerHTML = 'O numero é maior';
@@ -42,15 +43,17 @@ if(numberDrawn == kick){
     clear();
 } else if (numberDrawn < kick){ 
     status.innerHTML = 'O numero é menor';
-
+}
 }
 
-};
-
 function  playAgain() {
-    document.getElementById('btnRestart').styles.display = 'flex';
-};
+    document.getElementById('btnRestart').style.display = 'flex';
+}
 
-function restart(){document.location.reload(true);};
+function restart(){
+    document.location.reload(true);
+}
 
-function clear() {document.getElementById('kick').value='';};
+function clear() {
+    document.getElementById('kick').value='';
+}
